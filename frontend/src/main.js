72c63e63b8823ab0fd8fcbb91cc960765e327687
felipecabesssa <felipesoares100@hotmail.com>
2022-03-28 19:21:32 -0300

    Instalando e importando Axios e Bootstrap

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -3,4 +3,14 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
-createApp(App).use(store).use(router).mount('#app')
+import bootstrap from './bootstrap'
+
+import axios from 'axios'
+
+createApp(App)
+.use(store)
+.use(router)
+.use(bootstrap)
+.provide(axios)
+.mount('#app')
+
