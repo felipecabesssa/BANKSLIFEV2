7c760819dcf48788368bdf8173b32893a3c018af
felipecabesssa <felipesoares100@hotmail.com>
2022-03-28 23:43:14 -0300

    Estilizando Categorias e Produtos

--- frontend/src/views/Categorias.vue
+++ frontend/src/views/Categorias.vue
@@ -1,10 +1,11 @@
 <template>
   <div>
-      <div class="categorias">
+      <div class="categoria">
         <h1>Categorias</h1>
 
-        <div class="list">
-            {{ categorias }}
+        <div class="list" v-for="categoria in categorias" :key="categoria.id">
+            <div class="dados">{{ categoria.id }}</div>
+            <div class="dados">{{ categoria.nome }}</div>
         </div>
 
       </div>
@@ -36,6 +37,23 @@ export default {
 
 </script>
 
-<style>
+<style lang="scss" scoped>
+
+.categoria{
+    align-items: center;
+    text-align: center;
+    .list{
+        width: 50%;
+        margin-left: 22.5%;
+        align-items: center;
+        justify-content: center;
+        padding: 1%;
+        display: flex;
+        border: 1px solid grey;
+        .dados{
+            padding: 1%;
+        }
+    }
+}
 
 </style>
--- frontend/src/views/Produtos.vue
+++ frontend/src/views/Produtos.vue
@@ -2,10 +2,23 @@
   <div class="principal">
       <div class="principal">
           <h1>Produtos</h1>
+
+          <ul>
+                  <li>
+                      <div class="dados" id="id">id</div>
+                      <div class="dados" id="nome">Nome</div>
+                      <div class="dados" id="estoque">Estoque</div>
+                      <div class="dados" id="preco">Preco</div> 
+                  </li>
+              </ul>
+
           <div v-for="dado in dados" :key="dado.id">
               <ul>
                   <li>
-                      {{ dado.id }} - {{ dado.nome }} - {{ dado.estoque }} - {{ dinheiro(dado.preco) }}
+                      <div class="dados" id="id">{{ dado.id }}</div>
+                      <div class="dados" id="nome">{{ dado.nome }}</div>
+                      <div class="dados" id="estoque">{{ dado.estoque }}</div>
+                      <div class="dados" id="preco">{{ dinheiro(dado.preco) }}</div> 
                   </li>
               </ul>
           </div>
@@ -41,6 +54,46 @@ export default {
 }
 </script>
 
-<style>
+<style lang="scss" scoped>
+  .principal{
+    padding: 1%;
+    padding-top: 0.1%;
+    margin-top: -1%;
+    ul{
+      list-style: none;
+      li{
+        display: flex;
+        justify-content: space-between;
+        border: solid 1px grey;
+      }
+      .dados{
+        padding: 0.5%;
+        border-left: solid 1px grey;
+        border-right: solid 1px grey;
+      }
+      #id{
+        min-width: 10%;
+        border-left: none;
+      }
+      #nome{
+        min-width: 50%;
+        color: #42b983;
+        border: none;
+      }
+      #descricao{
+        min-width: 50%;
+        border: none;
+      }
+      #estoque{
+        min-width: 10%;
+        min-width: 80px;
+      }
+      #preco{
+        min-width: 14%;
+        border-right: none;
+        min-width: 100px;
+      }
+    }
+  }
 
 </style>
